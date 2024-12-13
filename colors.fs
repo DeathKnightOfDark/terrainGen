e6a7d5e2409266f41a3486849767a523ce1bdc76
MastyuginDA <MastyuginDA@MASTYUGINDA>
2024-12-13 16:52:30 +0300

    oops

--- colors.fs
+++ colors.fs
@@ -1,4 +1,23 @@
 #version 330 core
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
 out vec4 FragColor;
 
 in vec3 Normal;  
@@ -6,20 +25,34 @@ in vec3 FragPos;
   
 uniform vec3 lightPos; 
 uniform vec3 lightColor;
-uniform vec3 objectColor;
+
+uniform vec3 viewPos;
+
+uniform Material material;
+uniform Light light;
+
+float near = 0.1;
+float far = 100.0;
+
+float LineriazeDepth(float depth) 
+{
+    float z = depth*2.0 - 1.0;
+    return (2.0*near*far)/(far+near-z*(far-near));
+}
+
 
 void main()
 {
-    // ambient
-    /*float ambientStrength = 0.9;
-    vec3 ambient = ambientStrength * lightColor;
-  	
-    // diffuse 
+   
+    vec3 colorBegin = vec3(0.5, 0.0, 0);
+    vec3 colorEnd = vec3(0.0,0.5,0.0);
+    vec3 colorDiff = colorEnd - colorBegin;
+   
+
+    vec3 fragPointColor = vec3(0.1, 0.1, 0.1)*(FragPos.y*0.6+0.2);
+    float depth = LineriazeDepth(gl_FragCoord.z)/far;
+
+    //FragColor = vec4(fragPointColor, 1.0);
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-            
-    vec3 result = (ambient + diffuse) * objectColor;*/
-    FragColor = vec4(objectColor, 1.0);
+    FragColor = vec4(norm, 1.0);
 } 
--- colors.vs
+++ colors.vs
@@ -1,5 +1,6 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
 
 
 out vec3 FragPos;
@@ -8,11 +9,14 @@ out vec3 Normal;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform float time;
 
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = vec3(1.0, 0.0,0.0);
+    //FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = aNormal;
     
+    
+    FragPos = vec3(model*vec4(aPos.x, aPos.y, aPos.z, 1.0));
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
